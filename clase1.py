81de29261f243503bc217df2319b0f8aafac8263
Mau Munguia <maumg1196@gmail.com>
2020-04-11 15:55:03 -0500

    Add class 1, numbers and operators

---
+++ clase1.py
@@ -0,0 +1,72 @@
+"""Clase1: Numeros."""
+
+# Enteros
+print(1)
+print('Tipo de dato: ', type(1))
+
+# Enteros Binarios
+print(0b10)
+print('Tipo de dato: ', type(0b10))
+
+# Enteros Octal
+print(0o10)
+print('Tipo de dato: ', type(0o10))
+
+# Enteros Hexadecimal
+print(0x10)
+print('Tipo de dato: ', type(0x10))
+
+# Flotante
+print(-7.86)
+print('Tipo de dato: ', type(-7.86))
+
+# Complejo
+print(-4.25 + 8j)
+print('Tipo de dato: ', type(-4.25 + 8j))
+
+# Suma
+print('Suma de 5+7', 5+7)
+print('Tipo de dato', type(5+7))
+
+print('Suma de 5.0+7', 5.0+7)
+print('Tipo de dato', type(5.0+7))
+
+print('Suma de 5.0+7.0', 5.0+7.0)
+print('Tipo de dato', type(5.0+7.0))
+
+# Resta
+print('Resta de 5-7', 5-7)
+print('Tipo de dato', type(5-7))
+
+print('Resta de 5.0-7', 5.0-7)
+print('Tipo de dato', type(5.0-7))
+
+print('Resta de 5.0-7.0', 5.0-7.0)
+print('Tipo de dato', type(5.0-7.0))
+
+# Multiplicacion
+print('Multiplicacion de 5*7', 5*7)
+print('Tipo de dato', type(5*7))
+
+print('Multiplicacion de 5.0*7', 5.0*7)
+print('Tipo de dato', type(5.0*7))
+
+print('Multiplicacion de 5.0*7.0', 5.0*7.0)
+print('Tipo de dato', type(5.0*7.0))
+
+# Division
+print('Division de 5/5', 5/5)
+print('Tipo de dato', type(5/5))
+
+print('Division de 5.0/5', 5.0/5)
+print('Tipo de dato', type(5.0/5))
+
+print('Division de 5.0/5.0', 5.0/5.0)
+print('Tipo de dato', type(5.0/5.0))
+
+# Division Trunca
+print('Division de 5/7', 5/7)
+print('Tipo de dato', type(5/7))
+
+print('Division Trunca de 5//7', 5.0//7)
+print('Tipo de dato', type(5+7))
